9049b83106ba26893cea5f0224d93a4733c17695
Mister-webdev <Mister-webdev@x.com>
2021-01-06 03:56:07 +0600

    1

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    margin: 0;
     padding: 0;
+    margin: 0;
     box-sizing: border-box;
   }
 `;
