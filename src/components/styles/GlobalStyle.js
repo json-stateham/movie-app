9a3d223954d0ba8327d9bf5072064af6e7f6fcc7
Mister-webdev <roman.pogo@yandex.ru>
2021-01-06 04:04:34 +0600

    (no branch): Auto stash before cherry pick of "test"
    1

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    padding: 0;
     margin: 0;
+    padding: 0;
     box-sizing: border-box;
   }
 `;
