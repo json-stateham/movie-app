10ea3b49c58c0eabea00457f04f4591b7c026a39
json_stateham <roman.pogo@yandex.ru>
2022-06-18 04:29:13 +0600

    const enum

--- src/shared/config/images.ts
+++ src/shared/config/images.ts
@@ -1,11 +1,11 @@
-enum IMAGE_BACKDROP {
+const enum IMAGE_BACKDROP {
   S = 'w300',
   M = 'w700',
   L = 'w1280',
   ORIGINAL = 'original',
 }
 
-enum IMAGE_THUMB {
+const enum IMAGE_THUMB {
   XS = 'w92',
   S = 'w154',
   M = 'w185',
