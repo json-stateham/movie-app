d90a87730c7a3ec84254ff7bf25f7fec46d9aaab
json_stateham <roman.pogo@yandex.ru>
2022-08-11 05:38:24 +0600

    refactored apiConfig

--- lib/network/apiConfig.ts
+++ lib/network/apiConfig.ts
@@ -5,8 +5,8 @@ type TPath = Array<string>;
 type TParams = Record<string, string>;
 
 const CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL as string,
-  BASE_PARAMS: {
+  URL: process.env.NEXT_PUBLIC_BASE_URL as string,
+  OPTIONS: {
     api_key: process.env.API_KEY as string,
     language: 'en-US',
   },
@@ -15,18 +15,28 @@ const CONFIG = {
   },
 };
 
-const makePath = (...path: TPath) => `${CONFIG.BASE_URL}${path.join('/')}`;
+const makePath = (...path: TPath) => `${CONFIG.URL}${path.join('/')}`;
 
 const makeParams = (params: TParams = {}) =>
-  `?${serialUrlParams({ ...params, ...CONFIG.BASE_PARAMS })}`;
+  `?${serialUrlParams({ ...params, ...CONFIG.OPTIONS })}`;
 
-export const movieDetailsUrl = (movieId: number) =>
-  `${makePath('movie', movieId.toString())}${makeParams(CONFIG.APPEND_MOVIE_DETAILS)}`;
+export const movieDetailsUrl = (movieId: number, params: TParams = {}) =>
+  `${makePath('movie', movieId.toString())}${makeParams({
+    ...CONFIG.APPEND_MOVIE_DETAILS,
+    ...params,
+  })}`;
 
-export const makeMoviesListURL = (movieCategory: TMovieCategory, page: number) =>
+export const makeMoviesListURL = (
+  movieCategory: TMovieCategory,
+  page: number,
+) =>
   `${makePath('movie', movieCategory)}${makeParams({ page: page.toString() })}`;
 
-export const GENRES_URL = `${makePath('genre', 'movie', 'list')}${makeParams()}`;
+export const GENRES_URL = `${makePath(
+  'genre',
+  'movie',
+  'list',
+)}${makeParams()}`;
 
-// export const discoverMovies = () => 
+// export const discoverMovies = () =>
 // makePath('discover', 'movie') + makeParams({ page: '500', sort_by: 'release_date.desc' });
