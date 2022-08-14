f5e53e2eda76f87cc0e9984251757a5a23f359c2
json_stateham <roman.pogo@yandex.ru>
2022-08-15 04:28:43 +0600

    chaining for urlBuilder

--- api/movies/index.ts
+++ api/movies/index.ts
@@ -13,24 +13,24 @@ export const getMovieDetails = (
   movieId: string,
   params: Partial<TParams> = {},
 ) => {
-  const reqUrl = BASE_MOVIE_URL.clone();
-  reqUrl.addPath('movie', movieId);
-  reqUrl.addParams({ ...API_CONFIG.APPEND_MOVIE_DETAILS, ...params });
+  const reqUrl = BASE_MOVIE_URL.clone()
+    .addPath('movie', movieId)
+    .addParams({ ...API_CONFIG.APPEND_MOVIE_DETAILS, ...params });
   return jsonFetch(reqUrl.href);
 };
 
 export const getMoviesList = (movieCategory: TMovieCategory, page: number) => {
-  const reqUrl = BASE_MOVIE_URL.clone();
-  reqUrl.addPath('movie', movieCategory);
-  reqUrl.addParams({ page: String(page) });
+  const reqUrl = BASE_MOVIE_URL.clone()
+    .addPath('movie', movieCategory)
+    .addParams({ page: String(page) });
   return jsonFetch(reqUrl.href);
 };
 
 export const getGenres = () => {
-  const reqUrl = BASE_MOVIE_URL.clone();
-  reqUrl.addPath('genre', 'movie', 'list');
+  const reqUrl = BASE_MOVIE_URL.clone()
+    .addPath('genre', 'movie', 'list');
   return jsonFetch(reqUrl.href);
-}
+};
 
 // export const discoverMovies = () =>
 // makePath('discover', 'movie') + makeParams({ page: '500', sort_by: 'release_date.desc' });
--- lib/network/urlBuilder.ts
+++ lib/network/urlBuilder.ts
@@ -5,12 +5,14 @@ export class UrlBuilder extends URL {
 
   addPath(...path: string[]) {
     this.pathname = `${this.pathname}/${path.join('/')}`;
+    return this;
   }
 
   addParams(params: Record<string, string>) {
     Object.entries(params).forEach(([key, value]) => {
       this.searchParams.append(key, value);
     });
+    return this;
   }
 
   clone() {
