924201fadb55fde8710e5b1427cdb22319dda4cb
json_stateham <roman.pogo@yandex.ru>
2022-08-17 05:13:36 +0600

    small fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import Layout from 'components/layout/layout';
 import { ErrorBoundary } from 'entities/ErrorBoundary';
-import '@/styles/index.scss';
+import 'styles/index.scss';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ErrorBoundary>
